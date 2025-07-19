a35a453fba4622b73edf12bf2e158832f6b79463
Виктор <Irzunovv@gmail.com>
2025-07-19 16:31:26 +0300

    -5

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const phoneNumbers = {
+	phone1: '+375 33 000-00-00',
+	phone1Link: '+375330000000',
+	phone1Social: '375330000000',
+	phone2: '+375 29 863-30-00',
+	phone2Link: '+375298633000',
+ };
+ 
+ export default phoneNumbers;
+ 
